620e6bdd9cd2d3831ecb99a329825485af5d06fa
Marcos Paulo Machado <53479051+marcos010894@users.noreply.github.com>
2021-01-30 21:05:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
- # -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 from flask import Flask,request,jsonify
+from flask_cors import CORS
 from db import docDB
 import os
-from flask.ext.cors import CORS, cross_origin
 
 
 
-app = Flask(__name__)
-
-cors = CORS(app, resources={r"/foo": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+app = Flask(_name_)
+CORS(app)
 
 #############################################
 @app.route('/<string:arg>',methods = ['GET'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_Api_search(arg):
- 
     data = docDB()
 
-    false = [{"email" : False}]
+    false = [{"cert" : False}]
     search = [i for i in data if i["email"] == arg ]
     if(len(search)==0):
-        return jsonify(false[0])
+        return jsonify(false)
 
 
-    return jsonify(search[0])
+    return jsonify(search)
 
 
 
@@ -33,6 +29,6 @@ def get_Api_search(arg):
 
     # inicia o codigo
 #############################################
-if __name__== '__main__':
+if _name== 'main_':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True,host="0.0.0.0",port=port)
