e27cbfc50a4a91eb6a1533e59776903572cd5114
Marcos Paulo Machado <53479051+marcos010894@users.noreply.github.com>
2021-02-18 10:20:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 from flask import Flask,request,jsonify
 from flask_cors import CORS
-from db import docDB
 import os
 
 
@@ -12,15 +11,37 @@ CORS(app)
 #############################################
 @app.route('/<string:arg>',methods = ['GET'])
 def get_Api_search(arg):
-    data = docDB()
 
-    false = [{"status" : False}]
-    search = [i for i in data if i["email"] == arg ]
-    if(len(search)==0):
-        return jsonify(false[0])
+    false = [{"pago" : True}]
+  
+    return jsonify(false[0])
 
 
-    return jsonify(search[0])
+
+
+
+
+    # inicia o codigo
+#############################################
+if __name__== '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True,host="0.0.0.0",port=port) # -*- coding: utf-8 -*-
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import os
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+#############################################
+@app.route('/<string:arg>',methods = ['GET'])
+def get_Api_search(arg):
+
+    false = [{"pago" : True}]
+  
+    return jsonify(false[0])
 
 
 
@@ -32,4 +53,3 @@ def get_Api_search(arg):
 if __name__== '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True,host="0.0.0.0",port=port)
-
